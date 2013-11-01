15f9216f43658d9e84041b0c5e799502d779d738
Josh Williams <josh.williams@nytimes.com>
2013-11-01 12:52:34 -0400

    added basic task code

--- tasks/responsive_videos.js
+++ tasks/responsive_videos.js
@@ -8,43 +8,125 @@
 
 'use strict';
 
+var _ = require('underscore'),
+    async = require('async'),
+    path = require('path'),
+    ffmpeg = require('ffmpeg-node');
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    var DEFAULT_OPTIONS = {
+        separator: '-',
+        sizes: [{
+            name: 'small',
+            width: 640
+        }]
+    };
 
-  grunt.registerMultiTask('responsive_videos', 'Generate videos at various sizes', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    // check if there are any items in our array
+    function isValidArray(obj) {
+        return (_.isArray(obj) && obj.length > 0);
+    }
+
+
+    // check whether we've been given any valid size values
+    function isValidSize(obj) {
+        return (_.isNumber(obj.width) || _.isNumber(obj.height));
+    }
+
+
+    // create a name to suffix to our file.
+    function getName(name, width, height, separator, suffix) {
+
+        // handle empty separator as no separator
+        if (typeof separator === 'undefined') {
+            separator = '';
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        // handle empty suffix as no suffix
+        if (typeof suffix === 'undefined') {
+            suffix = '';
+        }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        if (name) {
+            return separator + name + suffix;
+        } else {
+            if (width && height) {
+                return separator + width + 'x' + height + suffix;
+            } else {
+                return separator + (width || height) + suffix;
+            }
+        }
+    }
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    grunt.registerMultiTask('responsive_videos', 'Videos at various resonsive sizes', function() {
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var that = this;
+        var done = this.async();
+        var series = [];
+        var options = this.options(DEFAULT_OPTIONS);
+        var tally = {};
+
+        if (!isValidArray(options.sizes)) {
+            return grunt.fail.warn('No sizes have been defined.');
+        }
+
+        options.sizes.forEach(function(s) {
+
+            // consts
+            var DEFAULT_SIZE_OPTIONS = {
+                quality: 1
+            };
+
+            // variable
+            var sizeOptions = _.clone(_.extend(DEFAULT_SIZE_OPTIONS, s));
+            var sizingMethod = 'resize';
+
+            if (!isValidSize(s)) {
+                return grunt.fail.warn('Size is invalid');
+            }
+
+            // use crop if both width and height are specified.
+            if (s.width && s.height) {
+                sizingMethod = 'crop';
+            }
+
+            // create a name suffix for our image
+            sizeOptions.name = getName(s.name, s.width, s.height, options.separator, s.suffix);
+
+            tally[sizeOptions.name] = 0;
+
+
+            that.files.forEach(function(f) {
+
+                var extName = path.extname(f.dest),
+                    srcPath = f.src[0],
+                    baseName = path.basename(srcPath, extName), // filename without extension
+                    dirName = path.dirname(f.dest),
+                    dstPath = path.join(dirName, baseName + sizeOptions.name + '.mp4');
+
+                console.log(extName, srcPath, dirName, baseName, dstPath);
+
+                // series.push(function(callback) {
+                //     im[sizingMethod](imageOptions, function(error, stdout, stderr) {
+                //         if (error) {
+                //             grunt.fail.warn(error.message);
+                //         } else {
+                //             grunt.verbose.ok('Responsive Image: ' + srcPath + ' now ' + dstPath);
+                //             tally[sizeOptions.name]++;
+                //         }
+                //         return callback();
+                //     });
+
+
+                // });
+
+                ffmpeg.exec(['-i', srcPath, '-vcodec', 'libx264', '-acodec', 'libfaac', '-pix_fmt', 'yuv420p', '-q:v', '4',  '-q:a', '100', '-threads', '0', dstPath], function() {
+                    done.apply();
+                });
+            });
+        });
     });
-  });
-
 };
